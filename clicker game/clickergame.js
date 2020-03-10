f98d960c4975cbd2051a4c788d166d1b03274a42
Louwrens-de-Palingboer <60880777+Louwrens-de-Palingboer@users.noreply.github.com>
2020-03-10 13:37:54 +0100

    cesna vliegtuig voor moneyPerSecond

--- clicker game/clickergame.js
+++ clicker game/clickergame.js
@@ -2,21 +2,25 @@ let img;
 let Money;
 let swordfish;
 let moneyPerClick;
+let moneyPerSecond;
 
 function preload()
 {
   img = loadImage('assets/sky.jpg');
   swordfish = loadImage('assets/swordfish.jpg');
+  cesna = loadImage('assets/cesna.jpg');
 }
 
 
 function setup() {
-  createCanvas(1080, 720);
+  createCanvas(1400, 720);
   fill(255, 0, 0);
   image(img, 0, 0, 1080, 720);
   image(swordfish, 400, 250, 250, 200);
   Money = 0;
   moneyPerClick = 1;
+  moneyPerSecond = 0;
+  image(cesna, 1100, 50, 300, 200);
 }
 
 function draw()
@@ -24,6 +28,7 @@ function draw()
   fill(255, 0, 0);
   image(img, 0, 0, 1080, 720);
   image(swordfish, 400, 250, 250, 200);
+  image(cesna, 1100, 50, 300, 200);
   textSize(32);
   text('Money: ' + Money, 460, 40);
 }
