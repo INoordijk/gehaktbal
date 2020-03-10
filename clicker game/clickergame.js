260cfe289e4ce3595c4e26ce5205a5dda31edbd9
Shadowerr <50573831+Shadowerr@users.noreply.github.com>
2020-03-10 11:59:01 +0100

    added upgreat and M

--- clicker game/clickergame.js
+++ clicker game/clickergame.js
@@ -2,11 +2,14 @@ let img;
 let Money;
 let swordfish;
 let moneyPerClick;
+let playerLvl;
+let playerClick;
 
 function preload()
 {
   img = loadImage('assets/sky.jpg');
   swordfish = loadImage('assets/swordfish.jpg');
+  upgreat = loadImage ('assets/upgreat.webp');
 }
 
 
@@ -17,6 +20,9 @@ function setup() {
   image(swordfish, 400, 250, 250, 200);
   Money = 0;
   moneyPerClick = 1;
+  image(upgreat, 300,300,300,300);
+  playerClick = 1;
+  playerLvl = 1;
 }
 
 function draw()
@@ -25,7 +31,19 @@ function draw()
   image(img, 0, 0, 1080, 720);
   image(swordfish, 400, 250, 250, 200);
   textSize(32);
-  text('Money: ' + Money, 460, 40);
+  if(Money < 1000000){
+      text('Money: ' + Money, 460, 40);
+   }
+  else if(Money <= 1000000000 && Money >= 1000000){
+      
+      
+      text('Money: ' + (Money / 1000000) + 'M', 460, 40);
+  }
+    else {
+        text('Big pepe', 460, 40);
+    }
+  image(upgreat, 30,100,100,100);
+  
 }
 
 function mouseClicked()
@@ -34,5 +52,16 @@ function mouseClicked()
   {
     Money += moneyPerClick;
     
+  }
+  if(mouseX > 30 && mouseX < 130 && mouseY > 100 && mouseY < 200)
+  {
+    playerLvl++;
+    playerClick = playerLvl * 7;
+    moneyPerClick += playerClick;
+    
+      console.log(moneyPerClick);
+      console.log(playerLvl);
+      console.log(playerClick);
+      
   }
 }
