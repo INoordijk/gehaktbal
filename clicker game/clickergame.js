a99171bfab8521c12356bea0b70461067192d08d
Louwrens-de-Palingboer <60880777+Louwrens-de-Palingboer@users.noreply.github.com>
2020-03-10 11:07:11 +0100

    vliegtuig + punten increase als je op de vliegtuig plaatje klikt

--- clicker game/clickergame.js
+++ clicker game/clickergame.js
@@ -1,22 +1,35 @@
 let img;
-let score = 0;
+let Money;
+let swordfish;
+let moneyPerClick;
 
 function preload()
 {
   img = loadImage('assets/sky.jpg');
+  swordfish = loadImage('assets/swordfish.jpg');
 }
 
 
 function setup() {
   createCanvas(1080, 720);
   fill(255, 0, 0);
-  image(img, 0, 0);
+  image(img, 0, 0, 1080, 720);
+  image(swordfish, 400, 250, 250, 200);
+  Money = 0;
+  moneyPerClick = 1;
 }
 
 function draw()
 {
   textSize(32);
-  text('score: ' + score, 460, 40);
+  text('Money: ' + Money, 460, 40);
 }
 
-
+function mouseClicked()
+{
+  if(mouseX > 400 && mouseX < 650 && mouseY > 250 && mouseY < 450)
+  {
+    Money = Money += moneyPerClick;
+    
+  }
+}
